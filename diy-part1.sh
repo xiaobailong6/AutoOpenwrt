9e83a5c2fd670b15d56f37ae2cc74d38aab46424
XBaiLong <2995773594@qq.com>
2020-11-22 10:46:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,18 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git https://github.com/cnzd/luci-app-koolproxyR.git package/koolproxyR
+sed -i '$a src-git https://github.com/kenzok8/small.git package/small
+sed -i '$a src-git https://github.com/kenzok8/openwrt-packages.git package/kenzo
+sed -i '$a src-git https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+sed -i '$a src-git https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/openwrt-packages/brook
+sed -i '$a src-git https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/openwrt-packages/chinadns-ng
+sed -i '$a src-git https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/openwrt-packages/trojan-go
+sed -i '$a src-git https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/openwrt-packages/trojan-plus
+sed -i '$a src-git https://github.com/xiaorouji/openwrt-package/trunk/package/ssocks package/openwrt-packages/ssocks
+sed -i '$a src-git https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
+sed -i '$a src-git https://github.com/siropboy/mypackages/trunk/luci-app-autopoweroff package/openwrt-packages/luci-app-autopoweroff
+sed -i '$a src-git https://github.com/siropboy/mypackages/trunk/luci-app-control-timewol package/openwrt-packages/luci-app-control-timewol
+sed -i '$a src-git https://github.com/binge8/luci-theme-argon-mc.git package/openwrt-packages/luci-theme-argon-mc
+sed -i '$a src-git https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/openwrt-packages/luci-theme-opentomcat
+sed -i '$a src-git https://github.com/binge8/luci-theme-butongwifi.git package/openwrt-packages/luci-theme-butongwifi
